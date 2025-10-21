fd1760c5dbb3bb3baa9522e38ff2de7cbb9be2d1
Raul <raulyecla88@gmail.com>
2025-10-21 21:26:56 +0200

    actualizacion prueba

--- src/components/admin/ProductsTable.tsx
+++ src/components/admin/ProductsTable.tsx
@@ -522,8 +522,12 @@ export default function ProductsTable({ initialProducts, categories }: Props) {
                         title="Eliminar producto"
                         aria-label="Eliminar producto"
                       >
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-5 w-5">
-                          <path d="M9 3.75a.75.75 0 0 1 .75-.75h4.5a.75.75 0 0 1 .75.75V5h4a.75.75 0 0 1 0 1.5h-1.052l-1.05 12.6A2.25 2.25 0 0 1 14.658 21H9.342a2.25 2.25 0 0 1-2.29-1.9L6.002 6.5H5a.75.75 0 0 1 0-1.5h4V3.75ZM8.507 6.5l1.05 12.6c.04.48.44.85.915.85h5.186a.95.95 0 0 0 .915-.85l1.05-12.6H8.507Z"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" className="h-5 w-5">
+                          <path d="M3 6h18" />
+                          <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+                          <path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6" />
+                          <path d="M10 11v6" />
+                          <path d="M14 11v6" />
                         </svg>
                       </button>
                     </div>
